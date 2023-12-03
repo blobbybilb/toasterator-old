fedb4d487a8e6edb744bf6f7921bb119beb69366
blobbybilb <blobbybilb@icloud.com>
2023-12-02 22:50:02 -0800

    save

--- server2.ts
+++ server2.ts
@@ -1,7 +1,26 @@
+import { exec } from 'child_process'
 import { Server } from './f4/server'
-import { Page, Heading, Text, Button, Buttons, Space, Form, Input, Alert, Select, Option, Submit } from './f4/ui'
+import { Page, Heading, Text, Space } from './f4/ui'
 
 const server = new Server()
+
+let isToasted = true
+let isUserDisabled = true
+const toastlist = 'java'
+const user = 'ronan'
+
+const commands = {
+  exit: () => exec('killall java'),
+  toast: () => (isToasted = true),
+  untoast: () => (isToasted = false),
+  disable: () => (isUserDisabled = true),
+  enable: () => (isUserDisabled = false),
+}
+
+setInterval(() => {
+  if (isToasted) exec('killall ' + toastlist)
+  if (isUserDisabled) exec('pkill -u ' + user)
+}, 30000)
 
 const mainPage = Page(
   'Toasterator',
