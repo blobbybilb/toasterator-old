5a0b8b4afdf1d0ee5cb25eeb466de1717ecb0179
blobbybilb <blobbybilb@skiff.com>
2023-12-25 13:24:59 -0800

    save

--- server2.ts
+++ server2.ts
@@ -1,13 +1,13 @@
 import { exec } from 'child_process'
 import { Server } from './f4/server'
-import { Page, Heading, Text, Space } from './f4/ui'
+import { Page, Heading, Text, Space, Button } from './f4/ui'
 
 const server = new Server()
 
 let isToasted = true
 let isUserDisabled = true
 const toastlist = 'java'
-const user = 'ronan'
+const user = 'rblob'
 
 const commands = {
   exit: () => exec('killall java'),
@@ -24,15 +24,23 @@ setInterval(() => {
 
 const mainPage = Page(
   'Toasterator',
-  [Heading('🍞🔥'), Space(), Text('helsjdhkajhskhasdlo'), Text('helsjdhkajhskhasdlo'), Text('helsjdhkajhskhasdlo')],
+  [Heading('🍞🔥'), Space(), Text('helsjdhkajhskhasdlo'), Text('helsjdhkajhskhasdlo'), Text('helsjdhkajhskhasdlo'), Button('Toast', '/do/toast')],
   'flatly',
 )
+
+const donePage = Page('Done - Toasterator', [], 'flatly')
 
 server.auth('/do', [{ user: 'blob', pass: 'wafflewaffle123' }])
 
 server.addRoutes({
   '/': mainPage,
-  '/do/a': 'yay',
+  '/do/*': (c) => {
+    const command = c.path[1]
+    if (!Object.keys(commands).includes(command)) return 'Error'
+    else (commands as any)[command]()
+
+    return donePage
+  },
 })
 
 export default server
